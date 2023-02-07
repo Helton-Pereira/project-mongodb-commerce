619f702abe6db8e98a7de59cd7eb51f6b9d25952
Helton-Pereira <helton.bfsm@gmail.com>
2023-02-07 16:10:33 -0300

    Req 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+db.produtos.find(
+    {
+      "valoresNutricionais.tipo": "calorias",
+      "valoresNutricionais.0.quantidade": { $lt: 500 },
+    },
+    {
+      _id: 0,
+      nome: 1,
+    },
+  ).pretty();
